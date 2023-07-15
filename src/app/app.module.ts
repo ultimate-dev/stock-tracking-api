e2e31228c16ef9bcadff1e6c89fa346269f3cee0
ulimate-dev <137053406+tahab-idealdata@users.noreply.github.com>
2023-07-15 18:32:04 +0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { PrismaService } from 'db/prisma.service';
+// App
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from 'db/prisma.service';
+// Api
 
 @Module({
   imports: [
