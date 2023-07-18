de641bd58516dbc10b6b416c4022cec9a627874a
ulimate-dev <137053406+tahab-idealdata@users.noreply.github.com>
2023-07-18 18:22:41 +0300

    ...

--- src/api/stocks/stock.controller.ts
+++ src/api/stocks/stock.controller.ts
@@ -8,7 +8,7 @@ import {
   Body,
   Param,
   UseGuards,
-  Request
+  Request,
 } from '@nestjs/common';
 import { StockService } from './stock.service';
 import { AuthGuard } from '@nestjs/passport';
@@ -17,24 +17,4 @@ import { AuthGuard } from '@nestjs/passport';
 @UseGuards(AuthGuard('jwt'))
 export class StockController {
   constructor(private readonly service: StockService) {}
-
-  @Get()
-  async getAll(@Request() req) {
-    try {
-      let { total, stocks } = await this.service.get();
-
-      return {
-        statusCode: 200,
-        status: true,
-        message: 'Success',
-        data: { total, stocks },
-      };
-    } catch (error) {
-      console.error(error);
-    }
-    throw new HttpException(
-      'Internal server error',
-      HttpStatus.INTERNAL_SERVER_ERROR,
-    );
-  }
 }
--- src/api/stocks/stock.service.ts
+++ src/api/stocks/stock.service.ts
@@ -4,15 +4,4 @@ import { PrismaService } from 'db/prisma.service';
 @Injectable()
 export class StockService {
   constructor(private prisma: PrismaService) {}
-
-  async get(fields?: any) {
-    const where = { ...fields };
-
-    let total = await this.prisma.stock.count(where);
-    let stocks = await this.prisma.stock.findMany(where);
-    return {
-      total,
-      stocks,
-    };
-  }
 }
